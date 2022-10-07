a639d89843622d439ef29e370753eb804cb00d59
salvador2104 <112212072+salvador2104@users.noreply.github.com>
2022-10-07 01:13:20 -0700

    Add files via upload

---
+++ test_average.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+int main(int argc, char const *argv[]){
+  std::vector<double> scores {};
+  int add_score;
+  std::cout << "Enter a test score: ";
+  if(add_score >= 0){
+    double entered_score = 0;
+    cin << entered_score;
+    std::scores.push_back(entered_score);
+  }
+  for(int i=0; i < scores.size(); ++i){
+    std::cout << scores.at(i) << "";
+  }
+  return = 0;
+}
